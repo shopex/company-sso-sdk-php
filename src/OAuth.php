d0b1dc2b65a204dcf01928bdcd2c5d2278b5d1de
Elrond <guocheng@shopex.cn>
2018-09-03 18:53:22 +0800

    udpate composer

---
+++ src/OAuth.php
@@ -0,0 +1,37 @@
+<?php
+namespace Shopex\CompanySSO;
+
+class OAuth
+{
+
+    private $key = "";
+
+    private $secret = "";
+
+    public function __construct($key, $secret)
+    {
+        $this->key = $key;
+        $this->secret = $secret;
+    }
+
+    /****
+     * 在实用中，这个方法最好重写
+     */
+    public function oauth($code = null,$isWorkWechat = false)
+    {
+        if($code || $this->getCode())
+        {
+            $code = $code ? : $this->getCode();
+            return $this->codeToUserinfo($code);
+        }
+
+        $redirectUrl = $this->getOauthRedirectUrl();
+        return $this->redirect($redirectUrl);
+    }
+
+    public function getOauthRedirectUrl($redirectUrl = "", $state = "")
+    {
+
+    }
+
+}
